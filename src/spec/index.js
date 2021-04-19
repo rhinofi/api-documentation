6f0e367001c8375dc05f8b4ef17d7fdd1e65b830
Tommy <tommycodebox@gmail.com>
2021-04-19 14:49:31 +0200

    Add recursive schema parsing

--- src/spec/index.js
+++ src/spec/index.js
@@ -5,6 +5,20 @@ import swaggerOverlay from './swagger-overlay.json';
 import {preprocess} from './preprocess';
 import merge from 'lodash.merge';
 
+const parseRefs = (properties, definitions) => {
+  Object.keys(properties).forEach(propKey => {
+    // if sub param is object check its subtree
+    if (properties[propKey].type === 'object' && properties[propKey].properties) {
+      parseRefs(properties[propKey].properties, definitions)
+      // if has $ref, replace with definition
+    } else if (properties[propKey].$ref) {
+      properties[propKey] = definitions[properties[propKey].$ref.replace('#/definitions/', '')]
+      // also parse definition if it has subtrees
+      parseRefs(properties[propKey])
+    }
+  })
+}
+
 const conciliation = data => {
   const {paths, definitions} = data;
   Object.keys(paths).forEach(pathKey => {
@@ -17,6 +31,8 @@ const conciliation = data => {
       parameters.forEach(param => {
         if (param.schema && param.schema.$ref) {
           param.schema = definitions[param.schema.$ref.replace('#/definitions/', '')];
+
+          parseRefs(param.schema.properties, definitions)
         }
       });
 
